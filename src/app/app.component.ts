69f59c8ff7e7c21675ec9446e4c2416eecadbaec
Marvin <marvin@fretly.fr>
2022-09-11 13:09:10 +0200

    feat: redirect not completed logged user to the first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth0Service } from './modules/auth/auth.service';
+import { CredentialsService } from './modules/auth/credentials.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tattoo';
+
+  constructor(
+    public auth0: Auth0Service,
+    private credentials: CredentialsService,
+    private router: Router
+  ) {
+    this.redirectToFirstStep();
+  }
+
+  private redirectToFirstStep() {
+    if (!this.credentials.isAuthenticated()) {
+      this.auth0.isAuthenticated.subscribe((isLoggedToAuth0) => {
+        if (isLoggedToAuth0) {
+          this.router.navigate(['/first-step'], {
+            replaceUrl: true,
+          });
+        }
+      });
+    }
+  }
 }
