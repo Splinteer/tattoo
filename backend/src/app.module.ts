d4eafafd1140260b9bd355c518a05ee18b83bfaf
Marvin <sanchezmarvin38@gmail.com>
2023-03-22 11:24:20 +0100

    WIP

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopModule } from './shop/shop.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ShopModule],
+  imports: [
+    ShopModule,
+    AuthModule.forRoot({
+      connectionURI: 'http://localhost:3567',
+      // apiKey: <API_KEY(if configured)>,
+      appInfo: {
+        // Learn more about this on https://supertokens.com/docs/thirdparty/appinfo
+        appName: 'app',
+        apiDomain: 'http://localhost:3000',
+        websiteDomain: 'http://localhost:4200',
+        apiBasePath: '/auth',
+        websiteBasePath: '/auth',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
