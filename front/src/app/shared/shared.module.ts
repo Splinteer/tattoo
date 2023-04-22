5f2d4fb014f6bf1c536fc5806a5f2033b178d603
Marvin <sanchezmarvin38@gmail.com>
2023-04-22 17:04:13 +0200

    feat: add the translate module to the shared module

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedUiModule } from './ui/ui.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, SharedUiModule],
-  exports: [SharedUiModule],
+  exports: [SharedUiModule, TranslateModule],
 })
 export class SharedModule {}
