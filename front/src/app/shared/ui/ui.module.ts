179058a6c01ab420d165635062ed361a95b7b3e6
Marvin <sanchezmarvin38@gmail.com>
2023-04-23 19:18:35 +0200

    feat: better custom inputs

--- front/src/app/shared/ui/ui.module.ts
+++ front/src/app/shared/ui/ui.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HamburgerComponent } from './hamburger/hamburger.component';
 import { IconInputComponent } from './icon-input/icon-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UrlInputComponent } from './url-input/url-input.component';
+import { FileDropzoneComponent } from './file-dropzone/file-dropzone.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  declarations: [HamburgerComponent, IconInputComponent],
-  exports: [HamburgerComponent, IconInputComponent],
+  declarations: [
+    HamburgerComponent,
+    IconInputComponent,
+    UrlInputComponent,
+    FileDropzoneComponent,
+  ],
+  exports: [
+    HamburgerComponent,
+    IconInputComponent,
+    UrlInputComponent,
+    FileDropzoneComponent,
+  ],
 })
 export class SharedUiModule {}
