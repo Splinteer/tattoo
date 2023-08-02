2239e30b034f1b14644701c20d6dfbed1753b55d
Marvin <sanchezmarvin38@gmail.com>
2023-08-03 01:41:48 +0200

    feat(shared): time ago pipe

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { SharedUiModule } from './ui/ui.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { ResponsiveComponent } from './responsive/responsive.component';
 import { ResponsiveService } from './responsive/responsive.service';
+import { TimeAgoPipe } from './timeAgo.pipe';
 
 @NgModule({
-  declarations: [ResponsiveComponent],
+  declarations: [ResponsiveComponent, TimeAgoPipe],
   imports: [CommonModule, SharedUiModule],
-  exports: [SharedUiModule, TranslateModule],
+  exports: [SharedUiModule, TranslateModule, TimeAgoPipe],
   providers: [ResponsiveService],
 })
 export class SharedModule {}
