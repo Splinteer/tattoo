6811ce2f71ef388f021f24b2b2535c7e3774ea16
Marvin <sanchezmarvin38@gmail.com>
2023-08-06 00:41:10 +0200

    fix: angular deprecations

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -6,15 +6,22 @@ import { AuthComponent } from './auth.component';
 import { SupertokensService } from './supertokens.service';
 import { CredentialsService } from './credentials.service';
 
+export function initializeSupertokens(
+  supertokensService: SupertokensService
+): () => Promise<void> {
+  return () => supertokensService.initializeApp();
+}
+
 @NgModule({
   declarations: [AuthComponent],
   imports: [CommonModule, AuthRoutingModule],
   providers: [
     CredentialsService,
     {
-      provide: [APP_INITIALIZER],
-      useClass: SupertokensService,
-      deps: [CredentialsService],
+      provide: APP_INITIALIZER,
+      useFactory: initializeSupertokens,
+      multi: true,
+      deps: [SupertokensService],
     },
   ],
 })
