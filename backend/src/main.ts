d063200762e354a5e85c1d453bf71a3645c287ca
Marvin <sanchezmarvin38@gmail.com>
2023-09-22 17:37:56 +0200

    feat: add openapi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import supertokens from 'supertokens-node';
 import { SupertokensExceptionFilter } from './auth/auth.filter';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,14 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Artesa.io API')
+    .setDescription('This is the API for the Artesa.io app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document);
 
   app.useGlobalFilters(new SupertokensExceptionFilter());
 
--- backend/src/project/project.dto.ts
+++ backend/src/project/project.dto.ts
@@ -1,14 +1,18 @@
 import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ProjectPatchBodyDTO {
+  @ApiProperty()
   @IsOptional()
   @IsBoolean()
   is_paid?: boolean;
 
+  @ApiProperty()
   @IsOptional()
   @IsNumber()
   customer_rating?: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsNumber()
   shop_rating?: number;
